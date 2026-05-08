7596b4c1be46ec8e283e0baec070d679f30f4e2c
AProgrammerWhoPrograms <88908791+AProgrammerWhoPrograms@users.noreply.github.com>
2026-05-08 12:44:53 -0600

    beautify

--- cleaning_queries/cleaning.sql
+++ cleaning_queries/cleaning.sql
@@ -3,8 +3,13 @@
 CREATE SCHEMA clean;
 
 --Duplicamos la tabla raw
-CREATE TABLE clean.datos_transitocdmx (LIKE raw.datos_transitocdmx INCLUDING ALL);
-INSERT INTO clean.datos_transitocdmx SELECT * from raw.datos_transitocdmx;
+CREATE TABLE clean.datos_transitocdmx
+(
+    LIKE raw.datos_transitocdmx INCLUDING ALL
+);
+INSERT INTO clean.datos_transitocdmx
+SELECT *
+from raw.datos_transitocdmx;
 
 select *
 from clean.datos_transitocdmx;
@@ -23,25 +28,28 @@ SET dia = TRIM(dia);
 
 --El dia ya esta implicito en los atributos de fecha y no es necesario almacenarlo de manera independiente
 
-ALTER TABLE clean.datos_transitocdmx DROP COLUMN dia;
+ALTER TABLE clean.datos_transitocdmx
+    DROP COLUMN dia;
 
 
 --En origen hay datos raros como sábado, inconsistencias por tildes en las palabras policía, cámara y botón. Dice MI C4LLE en vez de MI CALLE
 SELECT DISTINCT origen
 FROM clean.datos_transitocdmx;
-    --Reemplazo las tildes
+--Reemplazo las tildes
 UPDATE clean.datos_transitocdmx
 SET origen = TRANSLATE(origen, 'ÁÉÍÓÚ', 'AEIOU');
 
 
 --Inconsistencias y datos raros en tipo_interseccion
+
 --Clasificacion de la vialidad hay eje vial y ejevial
+
+
 --sentido de circulación y unidad medica
 
 -- Contexto
-SELECT
-    alcaldia,
-    COUNT(*) AS total
+SELECT alcaldia,
+       COUNT(*) AS total
 FROM clean.datos_transitocdmx
 GROUP BY alcaldia
 ORDER BY total DESC;
