2b4e9b79963152c81878ecbbf9d1f1a664863117
AProgrammerWhoPrograms <88908791+AProgrammerWhoPrograms@users.noreply.github.com>
2026-05-08 14:18:36 -0600

    corregir fechas nivel dios

--- cleaning_queries/cleaning.sql
+++ cleaning_queries/cleaning.sql
@@ -1,5 +1,4 @@
 --Creamos esquema para limpiar los datos
-
 CREATE SCHEMA clean;
 
 --Duplicamos la tabla raw
@@ -15,8 +14,7 @@ select *
 from clean.datos_transitocdmx;
 
 SELECT *
-FROM clean.datos_transitocdmx
-WHERE id = 1;
+FROM clean.datos_transitocdmx;
 
 --Hacer todos los días mayúsculas
 UPDATE clean.datos_transitocdmx
@@ -42,10 +40,94 @@ SET origen = TRANSLATE(origen, 'ÁÉÍÓÚ', 'AEIOU');
 
 --Inconsistencias y datos raros en tipo_interseccion
 
+
+select distinct tipo_de_interseccion, count(tipo_de_interseccion)
+FROM clean.datos_transitocdmx
+group by tipo_de_interseccion;
+
+--Una interseccion decia AJUSCO, buscamos las calles y vimos que es interseccion tipo T
+UPDATE clean.datos_transitocdmx
+SET tipo_de_interseccion = 'T'
+WHERE tipo_de_interseccion LIKE 'AJUSCO';
+
+SELECT *
+    from clean.datos_transitocdmx
+        where tipo_de_interseccion LIKE 'CRUZO';
+
+--Otra interseccion decía CRUZO en vez de CRUZ (verificamos con Maps)
+
+    START TRANSACTION;
+UPDATE clean.datos_transitocdmx
+    SET tipo_de_interseccion = 'CRUZ'
+WHERE tipo_de_interseccion = 'CRUZO';
+COMMIT;
+
 --Clasificacion de la vialidad hay eje vial y ejevial
 
+--Corregir formato dia-mes invertido
 
---sentido de circulación y unidad medica
+
+
+
+--En algunos casos, mes y dia estaban intercambiados
+UPDATE clean.datos_transitocdmx
+SET fecha_captura =  make_date(
+    EXTRACT(YEAR FROM fecha_captura)::int,
+    EXTRACT(DAY FROM fecha_captura)::int, -- Day becomes Month
+    EXTRACT(MONTH FROM fecha_captura)::int -- Month becomes Day
+)
+  WHERE fecha_captura<fecha_evento
+AND NOT extract(DAY FROM fecha_captura) >12;
+
+--Despues de arreglar mes y dia, vimos que el año de algunas tuplas en fecha_captura era incorrecto entonces lo cambiamos
+UPDATE clean.datos_transitocdmx
+    SET fecha_captura = make_date(2019,
+                        EXTRACT(MONTH FROM fecha_captura)::int,
+                        EXTRACT(DAY FROM fecha_captura)::int)
+WHERE fecha_captura<fecha_evento
+AND NOT extract(DAY FROM fecha_captura) >12
+AND fecha_captura = '2018-01-04';
+
+--Por ultimo, quedaban 4 tuplas con fecha_captura y fecha_evento intercambiados
+
+UPDATE clean.datos_transitocdmx
+SET fecha_captura =  fecha_evento,
+    fecha_evento = fecha_captura
+WHERE fecha_captura<fecha_evento
+AND NOT extract(DAY FROM fecha_captura) >12;
+
+
+START TRANSACTION;
+ROLLBACK;
+Commit;
+
+SELECT id, fecha_evento,
+       fecha_captura
+FROM clean.datos_transitocdmx
+    WHERE fecha_captura<fecha_evento
+    AND extract(DAY FROM fecha_captura) >12
+;
+
+--Quedaban 4 donde fecha_captura, fecha_evento están flipeados
+UPDATE clean.datos_transitocdmx
+SET fecha_captura =  fecha_evento,
+    fecha_evento = fecha_captura
+WHERE fecha_captura<fecha_evento;
+--unidad medica
+
+--Nullificar sentidos de circulación ambiguos y corregir Typo
+
+UPDATE clean.datos_transitocdmx
+SET sentido_de_circulacion = 'P-O'
+WHERE sentido_de_circulacion LIKE 'P O';
+
+UPDATE clean.datos_transitocdmx
+SET sentido_de_circulacion = NULL
+WHERE sentido_de_circulacion IN ('N', 'NO', 'PO');
+
+SELECT *
+FROM clean.datos_transitocdmx
+WHERE sentido_de_circulacion LIKE 'PO';
 
 -- Contexto
 SELECT alcaldia,
