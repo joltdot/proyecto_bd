a2085f0aad29e285baeaa625d97f5f8eb4032182
joltdot <joltxyz@icloud.com>
2026-05-12 20:39:39 -0600

    Eliminar consultas del script de limpieza

--- pipeline_scripts/02_data_cleaning.sql
+++ pipeline_scripts/02_data_cleaning.sql
@@ -37,13 +37,6 @@ DROP COLUMN zona_vial;
 
 --DEPURACIÓN DE ATRIBUTOS SUCIOS
 
--- ATRIBUTO: origen
--- Verificación (sirve para ver si la columna de origen está limpia)
-SELECT origen, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY origen
-ORDER BY total DESC;
-
 --En origen hay datos raros como sábado, inconsistencias por tildes en las palabras policía, cámara y botón. Dice MI C4LLE en vez de MI CALLE
 
 --LIMPIEZA GENERAL DE DATOS
@@ -62,21 +55,17 @@ WHERE origen = 'CÁMARA';
 
 -- Homologar variantes relacionadas con 911.
 -- Aquí dejamos LLAMADA DEL 911 separada de 911 CDMX.
-START TRANSACTION;
 UPDATE clean.datos_transitocdmx
 SET origen = '911 CDMX'
 WHERE origen ILIKE '%911%';
-COMMIT;
 
 
 -- Homologar la categoría botón ya que mi calle es de acuerdo a los datos parte de la categiría de botón de auxilio
-START TRANSACTION;
 UPDATE clean.datos_transitocdmx
 SET origen = 'BOTON DE AUXILIO'
 WHERE origen ILIKE '%BOTON%'
    OR origen ILIKE '%BOTÓN%'
    OR origen ILIKE '%AUXILIO%';
-COMMIT;
 
 -- Homologar redes sociales.
 UPDATE clean.datos_transitocdmx
@@ -85,18 +74,13 @@ WHERE origen IN ('REDES','REDES SOCIALES');
 
 
 --Reemplazo de categorías sin sentido a NULL (ejemplo: sabado)
-START TRANSACTION;
 UPDATE clean.datos_transitocdmx
 SET origen = NULL
 WHERE origen IS NOT NULL
 AND origen ILIKE '%SABADO%';
-COMMIT;
-ROLLBACK;
-
 
 -- Atributos que se mencionaron menos de 10 veces pasan a categoría de OTROS
 -- No se nulearon valores como SD dado que pueda tratarse de algun acrónimo muy particular que represente una categoría válida para origen
-START TRANSACTION;
 UPDATE clean.datos_transitocdmx
 SET origen = 'OTROS'
 WHERE origen IN (
@@ -106,20 +90,6 @@ WHERE origen IN (
     GROUP BY origen
     HAVING COUNT(*) < 10
 );
-COMMIT;
-ROLLBACK;
-
-
-
-
-
--- ATRIBUTO : tipo_de_interseccion
--- Verificación (sirve para ver si la columna de tipo_de_interseccion está limpia)
-SELECT tipo_de_interseccion, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY tipo_de_interseccion
-ORDER BY total DESC;
-
 
 --Una interseccion decia AJUSCO, buscamos las calles y vimos que es interseccion tipo T
 UPDATE clean.datos_transitocdmx
@@ -129,38 +99,15 @@ WHERE tipo_de_interseccion LIKE 'AJUSCO';
 
 
 --Otra interseccion decía CRUZO en vez de CRUZ (verificamos con Maps)
-    START TRANSACTION;
 UPDATE clean.datos_transitocdmx
     SET tipo_de_interseccion = 'CRUZ'
 WHERE tipo_de_interseccion = 'CRUZO';
-COMMIT;
-
-
-
-
-
-
--- ATRIBUTO: clasificacion_de_la_vialidad 
--- Verificación (sirve para ver si la columna de clasificacion_de_la_vialidad está limpia)
-SELECT clasificacion_de_la_vialidad, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY clasificacion_de_la_vialidad
-ORDER BY total DESC;
 
 -- Homologar eje vial y ejevial
 
 UPDATE clean.datos_transitocdmx
     SET clasificacion_de_la_vialidad = 'EJE VIAL'
     WHERE clasificacion_de_la_vialidad = 'EJEVIAL';
-    
-   
-
--- ATRIBUTO: fecha_captura
--- Verificación (sirve para ver si la columna de fecha_captura está limpia)
-SELECT fecha_captura, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY fecha_captura
-ORDER BY total DESC;
 
 --En algunos casos, mes y dia estaban intercambiados
 UPDATE clean.datos_transitocdmx
@@ -169,7 +116,7 @@ SET fecha_captura =  make_date(
     EXTRACT(DAY FROM fecha_captura)::int, -- Day becomes Month
     EXTRACT(MONTH FROM fecha_captura)::int -- Month becomes Day
 )
-  WHERE fecha_captura<fecha_evento
+WHERE fecha_captura<fecha_evento
 AND NOT extract(DAY FROM fecha_captura) >12;
 
 --Despues de arreglar mes y dia, vimos que el año de algunas tuplas en fecha_captura era incorrecto entonces lo cambiamos
@@ -189,41 +136,16 @@ SET fecha_captura =  fecha_evento,
 WHERE fecha_captura<fecha_evento
 AND NOT extract(DAY FROM fecha_captura) >12;
 
-
-SELECT id, fecha_evento,
-       fecha_captura
-FROM clean.datos_transitocdmx
-    WHERE fecha_captura<fecha_evento
-    AND extract(DAY FROM fecha_captura) >12;
-
 --Quedaban 4 donde fecha_captura, fecha_evento están flipeados
 UPDATE clean.datos_transitocdmx
 SET fecha_captura =  fecha_evento,
     fecha_evento = fecha_captura
 WHERE fecha_captura<fecha_evento;
 
-
-
--- ATRIBUTO: interseccion_semaforizada
--- Verificación (sirve para ver si la columna de XXXX está limpia)
-SELECT interseccion_semaforizada, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY interseccion_semaforizada
-ORDER BY total DESC;
-
 -- Homologar N como NO
 UPDATE clean.datos_transitocdmx
 SET interseccion_semaforizada =  'NO'
 WHERE interseccion_semaforizada ILIKE '%N%';
-
--- ATRIBUTO: sentido_de_circulacion
--- Verificación (sirve para ver si la columna de sentido_de_circulacion está limpia)
-SELECT sentido_de_circulacion, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY sentido_de_circulacion
-ORDER BY total DESC;
-   
-   
 
 --Nullificar sentidos de circulación ambiguos y corregir Typo
 UPDATE clean.datos_transitocdmx
@@ -233,19 +155,6 @@ WHERE sentido_de_circulacion LIKE 'P O';
 UPDATE clean.datos_transitocdmx
 SET sentido_de_circulacion = NULL
 WHERE sentido_de_circulacion IN ('N', 'NO', 'PO');
-
-
--- ATRIBUTO: alcaldia
--- Verificación (sirve para ver si la columna de alcaldia está limpia)
-SELECT alcaldia, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY alcaldia
-ORDER BY total DESC;
-
-
-SELECT colonia
-FROM clean.datos_transitocdmx
-WHERE alcaldia = 'AV INSURGENTES';
 
 -- Gustavo A Madero está repetido
 UPDATE clean.datos_transitocdmx
@@ -258,36 +167,12 @@ WHERE alcaldia = 'AV INSURGENTES';
 
 
 
--- ATRIBUTO: unidad_medica_de_apoyo
--- Verificación (sirve para ver si la columna de unidad_medica_de_apoyo está limpia)
-SELECT zona_vial, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY zona_vial
-ORDER BY total DESC;
-
-
-
-SELECT DISTINCT unidad_a_cargo
-FROM clean.datos_transitocdmx
-WHERE unidad_a_cargo NOT SIMILAR TO ('%MX%');
-
-
-
--- ATRIBUTO: sector
--- Verificación (sirve para ver si la columna de sector está limpia)
-SELECT sector, COUNT(*) AS total
-FROM clean.datos_transitocdmx
-GROUP BY sector
-ORDER BY total DESC;
-
 --LIMPIEZA GENERAL DE DATOS
 --Borramos espacios innecesarios y pasamos todo a mayúsculas
 UPDATE clean.datos_transitocdmx
 SET sector = UPPER(TRIM(sector));
 
 --Corregimos los typos observados de los sectores y los agrupamos a su correspondiente valor
-START TRANSACTION;
-
 UPDATE clean.datos_transitocdmx
 SET sector = 'TLATELOLCO'
 WHERE sector IN ('TLALTELOLCO', 'TLATALOLCO');
@@ -383,9 +268,6 @@ WHERE sector IN ('GRANJA', 'GRNAJAS');
 UPDATE clean.datos_transitocdmx
 SET sector = NULL
 WHERE sector = 'SD';
-
-COMMIT;
--- ROLLBACK;
 
 --Se corrigieron únicamente errores ortográficos evidentes en sectores policiales. Categorías raras o de baja frecuencia se conservaron para evitar introducir agrupaciones artificiales o perder info.
 
